5bdcc75d4368281155bd2248d9bf21d8bec74509
snozza <andrewcsnead@gmail.com>
2017-06-22 22:13:40 +0100

    add dynamodb lookup

--- app.js
+++ app.js
@@ -3,37 +3,55 @@ require('dotenv').load({ silent: true });
 const express = require('express');
 const co = require('co');
 const bodyParser = require('body-parser');
-const Client = require('ssh2');
-const fs = require('fs');
+const AWS = require('aws-sdk');
 const path = require('path');
 const compression = require('compression');
 const config = require('./config');
 const { notFound, errorMiddleware } = require('./errors');
 
 const app = new express();
-app.use(compression());
-const connSettings = {
-  host: config.sftpHost,
-  port: config.sftpPort,
-  username: config.sftpUsername,
-  password: config.sftpPassword
-};
-const numbers = {};
 
+AWS.config.update({
+  accessKeyId: config.awsAccessKeyId,
+  secretAccessKey: config.awsSecretAccessKey,
+  region: config.awsRegion
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+app.use(compression());
 app.use(bodyParser.json());
 
 app.post('/search', (req, res, next) => {
   if (!Array.isArray(req.body)) {
     return next({ message: 'Must provide array of numbers', status: 400 })
   }
-  const results = req.body.map((num) => {
-    return {
-      number: num,
-      canCall: !numbers[num.replace(/\s/g, '')] || false
-    };
-  });
+  co(function* () {
+    const results = yield req.body.map((num) => {
+      const params = {
+        TableName: 'ft-email_platform_tps_lookup',
+        Key: {
+          phone: num.replace(/\s/g, '')
+        }
+      };
 
-  res.json({ results });
+      return docClient.get(params)
+        .promise()
+        .then((result) => {
+          return Promise.resolve({
+            number: num,
+            canCall: result ? false : true
+          });
+        })
+        .catch((err) => {
+          return Promise.reject(err);
+        });
+    });
+    res.json({ results });
+  }).catch((err) => {
+    console.log(err);
+    next(err);
+  });
 });
 
 app.use(express.static(`${__dirname}/dist`));
@@ -44,33 +62,6 @@ app.get('/', (req, res, next) => {
 app.use(notFound);
 app.use(errorMiddleware);
 
-
-const FILENAME = 'tps.dat';
-const conn = new Client();
-conn.on('ready', () => {
-  const moveFrom = `./${FILENAME}`;
-  const moveTo = `/tmp/${FILENAME}`;
-
-  conn.sftp((err, sftp) => {
-    if (err) {
-      throw err;
-    }
-
-    sftp.fastGet(moveFrom, moveTo, {}, (downloadErr) => {
-      if (downloadErr) {
-        throw downloadErr;
-      }
-
-      // turn into map for constant lookup
-      // trim spaces from numbers
-      fs.readFileSync(`/tmp/${FILENAME}`).toString().split(/\r?\n/).forEach((num, i) => {
-        numbers[num] = true;
-      });
-
-      app.listen(config.PORT, () => {
-        console.log(`App listening on ${config.PORT}`);
-      });
-
-    });
-  });
-}).connect(connSettings);
+app.listen(config.PORT, () => {
+  console.log(`App listening on port ${config.PORT}`);
+});
--- config.js
+++ config.js
@@ -5,3 +5,6 @@ config.sftpHost = process.env.SFTP_HOST;
 config.sftpPort = process.env.SFTP_PORT;
 config.sftpUsername = process.env.SFTP_USERNAME;
 config.sftpPassword = process.env.SFTP_PASSWORD;
+config.awsAccessKeyId = process.env.AWS_ACCESS_KEY;
+config.awsSecretAccessKey = process.env.AWS_SECRET_KEY;
+config.awsRegion = process.env.AWS_REGION || 'eu-west-1';
