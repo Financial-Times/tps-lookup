41bdd8d0dfd5bd56263c28e47095d5f325749721
snozza <andrewcsnead@gmail.com>
2017-06-27 09:37:29 +0100

    block non https calls

--- ensureHttps.js
+++ ensureHttps.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
-  if (req.header['x-forwarded-proto'] !== 'https') {
-    return res.redirect(`https://${req.header('host')}${req.url}`);
+  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] !== 'https') {
+    const err = new Error('Please use HTTPS when submitting data to this server');
+    err.status = 403;
+    return next(err);
   }
   return next();
 };
