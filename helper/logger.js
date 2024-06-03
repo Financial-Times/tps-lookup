70705a1fc66c29efa196274ec2dfbd5681e5d326
GeekGirl <32463353+juliaguida@users.noreply.github.com>
2024-06-03 18:45:06 +0100

    removed space

--- helper/logger.js
+++ helper/logger.js
@@ -1,7 +1,5 @@
 const { Logger, transforms } = require('@dotcom-reliability-kit/logger');
-
 // Added transform legacyMask to prevent any potential log of PII in the Heroku index.
-
 const logger = new Logger({
     withPrettifier: false,
 	logLevel: 'info',
