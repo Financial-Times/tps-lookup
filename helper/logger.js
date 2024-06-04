24db742d393e0641c6a4dbab2502cf7f4871e0e9
GeekGirl <32463353+juliaguida@users.noreply.github.com>
2024-06-04 15:02:36 +0100

    Update helper/logger.js
    Co-authored-by: MeRo <melanie.rogan@ft.com>

--- helper/logger.js
+++ helper/logger.js
@@ -1,5 +1,5 @@
 const { Logger, transforms } = require('@dotcom-reliability-kit/logger');
-// Added transform legacyMask to prevent any potential log of PII in the Heroku index.
+// Added transform legacyMask to prevent any potential log of PII in the Heroku index. As of June 2024 we do not log any PII (in this case phone numbers). This mask is here to future proof any future added logs from having this information
 const logger = new Logger({
     withPrettifier: false,
 	logLevel: 'info',
