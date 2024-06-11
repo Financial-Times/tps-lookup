6a091de84d9cf590678a7ed003daec9c8b750281
GeekGirl <32463353+juliaguida@users.noreply.github.com>
2024-06-11 13:57:14 +0100

    PR2 changes for the n-logger migration

---
+++ helper/logger.js
@@ -0,0 +1,14 @@
+const { Logger, transforms } = require('@dotcom-reliability-kit/logger');
+// Added transform legacyMask to prevent any potential log of PII in the Heroku index. As of June 2024 we do not log any PII (in this case phone numbers). This mask is here to future proof any future added logs from having this information
+const logger = new Logger({
+    withPrettifier: false,
+    logLevel: 'info',
+    transforms:[
+        transforms.legacyMask({
+            denyList: [ 'phone' ]
+        })
+    ]
+
+});
+
+module.exports = logger;
