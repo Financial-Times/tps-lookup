60d3070185e09376c4b7a7a1f8aabd76db58d16b
Melanie Rogan <melanie.rogan@ft.com>
2025-05-15 15:51:55 +0100

    bin off coverage noise

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ require('dotenv').config({ path: '.env.test' });
 module.exports = {
   testEnvironment: 'node', 
   testMatch: ['**/*.test.js'], 
-  collectCoverage: true, 
-  coverageDirectory: 'coverage', 
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'], 
   testTimeout: 20000,
 };
