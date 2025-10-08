2bdbc7eb25b629cce7a6eaee0bf46c50d7819e27
Ahmed Warsama <ahmedwarsama@live.co.uk>
2025-10-08 17:10:21 +0100

    add useful logs

--- errors.js
+++ errors.js
@@ -1,3 +1,5 @@
+const logger = require('./helper/logger');
+
 function notFound(req, res, next) {
   const err = new Error('Page Not Found');
   err.status = 404;
@@ -6,8 +8,22 @@ function notFound(req, res, next) {
 
 // eslint-disable-next-line
 function errorMiddleware(err, req, res, next) {
-  console.log(err);
   res.status(err.status || err.statusCode || 500);
+
+    const errorContext = {
+      message: err.message,
+      stack: err.stack,
+      request: {
+        method: req.method,
+        url: req.originalUrl,
+        body: req.body,
+        headers: {
+          'user-agent': req.headers['user-agent']
+        },
+      },
+  };
+  logger.error('Express error', errorContext);
+
   return res.json({
     message: err.message,
     errors: err.errors || []
