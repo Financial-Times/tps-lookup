6128901a9c0ec5896b5cec8485e85438606ad3db
Ahmed Warsama <ahmedwarsama@live.co.uk>
2025-10-09 07:21:02 +0100

    Add further logs

--- authenticate.js
+++ authenticate.js
@@ -1,10 +1,18 @@
 const { okta } = require('./okta.js');
 const oktaNoRedirect = okta.verifyJwts({redirect:false});
 const config = require('./config');
+const logger = require('./helper/logger'); 
 
 module.exports = (req, res, next) => {
   const apiKey = req.headers['x-api-key'];
   if (apiKey && apiKey === config.apiKey) {
+    logger.info('Authenticated via API key', {
+      method: req.method,
+      url: req.originalUrl,
+      ip: req.ip,
+      userAgent: req.headers['user-agent']
+    });
+    
     return next()
   }
 
