20d45a863d54df1d7a88743af3087948e1277d7e
Ahmed Warsama <ahmedwarsama@live.co.uk>
2025-10-20 21:13:25 +0100

    Move removefromDynamo and AddtoDynamo functionality to another file

--- src/aws/dynamo.js
+++ src/aws/dynamo.js
@@ -1,1 +1,44 @@
-// this will be the file i will put the dynamo bits in in my next PR
+	const AWS = require("aws-sdk");
+const logger = require("../../helper/logger.js");
+const config = require("../../config.js");
+const { AWS_REGION } = process.env;
+const docClient = new AWS.DynamoDB.DocumentClient({ region: AWS_REGION });
+
+async function addToDynamo(phone) {
+  try {
+    const params = {
+      TableName: config.tableName,
+      Item: {
+        phone: phone.trim(),
+      },
+    };
+    return docClient.put(params).promise();
+  } catch (error) {
+    logger.error({
+      event: "Failed to add phone number to DynamoDB",
+      type: "FAILED",
+      error: error,
+    });
+    throw error;
+  }
+}
+
+async function removeFromDynamo(phone) {
+  try {
+    const params = {
+      TableName: config.tableName,
+      Key: {
+        phone: phone.trim(),
+      },
+    };
+    return docClient.delete(params).promise();
+  } catch (error) {
+    logger.error({
+      event: "Failed to remove phone number from DynamoDB",
+      type: "FAILED",
+      error: error,
+    });
+    throw error;
+  }
+}
+module.exports = { addToDynamo, removeFromDynamo };
